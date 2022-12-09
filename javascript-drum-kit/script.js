0fb609855c78ae7d2324fc726c5bdbdfdc65157f
tomasbrezina <tomas.brezinaaa@gmail.com>
2022-12-09 18:52:57 +0100

    added function playSound()

--- javascript-drum-kit/script.js
+++ javascript-drum-kit/script.js
@@ -1,16 +1,17 @@
-window.addEventListener('keydown', function (e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return; // stop function when key value is not matching data-key
     audio.currentTime = 0; // rewind to the start
     audio.play();
-    key.classList.add('playing')
-
-})
+    key.classList.add('playing');
+}
 
 function removeTransition(e) {
-    console.log(e.propertyName !== 'transform') return;
+    if (e.propertyName !== 'transform') return; // if its not transform skip whole line
+    this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll(`.key`);
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
