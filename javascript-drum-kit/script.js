fcc5398244a9d9cdb8c4a4e7aed60f307253aed3
tomasbrezina <tomas.brezinaaa@gmail.com>
2022-12-09 18:43:09 +0100

    added removeTransition function

--- javascript-drum-kit/script.js
+++ javascript-drum-kit/script.js
@@ -1,4 +1,16 @@
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(audio)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return; // stop function when key value is not matching data-key
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    key.classList.add('playing')
+
 })
+
+function removeTransition(e) {
+    console.log(e.propertyName !== 'transform') return;
+}
+
+const keys = document.querySelectorAll(`.key`);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
