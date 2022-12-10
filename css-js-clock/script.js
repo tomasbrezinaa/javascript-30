9436984fb48d7f995419bc49ad02502e3119410e
tomasbrezina <tomas.brezinaaa@gmail.com>
2022-12-10 16:13:18 +0100

    fixed problem with hourHand queryselector

--- css-js-clock/script.js
+++ css-js-clock/script.js
@@ -1,6 +1,6 @@
 const secondHand = document.querySelector(".second-hand");
 const minHand = document.querySelector(".min-hand");
-const hourHand = document.querySelector("hour-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 
 function setDate() {
@@ -14,13 +14,9 @@ function setDate() {
     const secondDegrees = ((sec / 60) * 360) + 90;
     const hourDegrees = ((hour / 12) * 360) + 90;
 
-
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
     minHand.style.transform = `rotate(${minuteDegrees}deg)`;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-
-
-    console.log(hours);
 }
 
 setInterval(setDate, 1000);
